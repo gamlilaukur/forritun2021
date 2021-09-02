48f3fb5af9450113fab857e8473f05c2b63f4dd9
Saemhundur <sonuroskars@gmail.com>
2021-09-02 14:54:19 +0000

    Endurbaett

--- max_int.py
+++ max_int.py
@@ -5,16 +5,12 @@
     Þegar forritið slær inn neikvæða tölu prentar forritið hæstu tölu sem notandi sló inn """
     
 max_int = 0
-notquit = True
+num_int = 0
 
-while notquit:
+while num_int >= 0:
     num_int = int(input("Input a number: "))    # Do not change this line
     # Fill in the missing code
-    if num_int > 0:
-        if num_int > max_int:
-            max_int = num_int
-    else:
-        notquit = False
+    if num_int > max_int:
+        max_int = num_int
         
-
 print("The maximum is", max_int)    # Do not change this line
